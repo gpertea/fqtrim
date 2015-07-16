f74b8e3276091fff53df2dcf3ee46c0402d4a655
Geo Pertea <geo.pertea@gmail.com>
2015-07-16 10:59:54 -0400

    initial source commit

---
+++ make_src.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ver=$(fgrep '#define VERSION ' fqtrim.cpp)
+ver=${ver#*\"}
+ver=${ver%%\"*}
+pack=fqtrim-$ver
+echo "preparing $pack"
+echo "-------------------"
+/bin/rm -rf $pack
+/bin/rm -f $pack.tar.gz
+mkdir $pack
+mkdir $pack/gclib
+#assuming gclib is a symlink or a subdir under current directory
+#sed 's|../gclib|./gclib|' Makefile > $pack/Makefile
+libdir=fqtrim-$ver/gclib/
+cp -p Makefile LICENSE README fqtrim.cpp fqtrim-$ver/
+cp -p ./gclib/{GVec,GList,GHash}.hh $libdir
+cp -p ./gclib/{GAlnExtend,GArgs,GBase,gdna,GStr,GThreads}.{h,cpp} $libdir
+tar cvfz $pack.tar.gz $pack
+scp $pack.tar.gz igm3:~/src/
